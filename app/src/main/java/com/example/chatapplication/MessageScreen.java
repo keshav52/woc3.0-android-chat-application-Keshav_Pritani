eba336fc2252a73f2de0c921655a6a6382583dda
keshav52 <73228599+keshav52@users.noreply.github.com>
2021-01-08 18:30:23 +0530

    Added Message And Chat Screens

---
+++ app/src/main/java/com/example/chatapplication/MessageScreen.java
@@ -0,0 +1,34 @@
+package com.example.chatapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.animation.Animation;
+
+import com.gigamole.navigationtabstrip.NavigationTabStrip;
+
+public class MessageScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_screen);
+        final NavigationTabStrip navigationTabStrip = (NavigationTabStrip) findViewById(R.id.nts);
+//        navigationTabStrip.setTitles("Chats", "Contacts", "Favorite");
+        navigationTabStrip.setTabIndex(0, true);
+       /* navigationTabStrip.setTitleSize(50);
+        navigationTabStrip.setStripColor(Color.RED);
+        navigationTabStrip.setStripWeight(6);
+        navigationTabStrip.setStripFactor(2);
+        navigationTabStrip.setStripType(NavigationTabStrip.StripType.LINE);
+        navigationTabStrip.setStripGravity(NavigationTabStrip.StripGravity.BOTTOM);
+        navigationTabStrip.setTypeface("fonts/typeface.ttf");
+        navigationTabStrip.setCornersRadius(3);
+        navigationTabStrip.setAnimationDuration(300);
+        navigationTabStrip.setInactiveColor(Color.GRAY);
+        navigationTabStrip.setActiveColor(Color.WHITE);*/
+//        navigationTabStrip.setOnPageChangeListener(...);
+//        navigationTabStrip.setOnTabStripSelectedIndexListener(...);
+    }
+}
